51e1d832f7a7bc554454c62f3d3b25db1fd2b5fe
vennica <vennica0521@gmail.com>
2024-04-09 00:17:32 +0800

    implemented delete and

--- public/login.js
+++ public/login.js
@@ -2,6 +2,8 @@ var accessToken = "";
 
 document.getElementById('login-form').addEventListener('submit', userLogin);
 document.getElementById('new_user-form').addEventListener('submit', userCreate);
+document.getElementById('deleteAccount').addEventListener('click', deleteUserAccount);
+document.getElementById('logout').addEventListener('click', logout);
 
 async function userLogin(event) {
     event.preventDefault();
@@ -25,6 +27,7 @@ async function userLogin(event) {
             accessToken = responseData.accessToken;
             alert('login successful');
             displayUponLogin();
+            getUserProfile(username,accessToken);
         } else {
             alert('login failed');
         }
@@ -65,4 +68,73 @@ async function userCreate(event) {
         console.error('Error logging in: ', error);
         alert('An error occurred while logging in. Please try again.');
     }
+}
+
+
+
+async function getUserProfile(email,accessToken){
+
+    //const email = user.email; 
+    console.log('email',email);
+    console.log('accessToken',accessToken);
+
+    var data = {
+        email: email
+    }
+
+    try {
+        const response = await fetch('http://localhost:3001/users/getUserByEmail', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + accessToken
+            },
+            body: JSON.stringify(data)
+        }).then(function(response) {
+            return response.json();
+          }).then(function(data) {
+            if(data.error){
+                alert("Couldn't retrieve profile");
+            } else {
+                console.log(data); 
+                alert('user profile retrieved!')
+                document.getElementById('userName').value = data?.userDetails?.email;
+                document.getElementById('createAt').value = data?.userDetails?.createdAt;
+            }
+          });
+    } catch (error) {
+        console.error('Error fetching profile: ', error);
+        // alert('An error occurred while loading profile. Please try again.');
+    }
+
+}
+
+async function deleteUserAccount(event){
+    console.log('handleDeleteUser',event);
+    const email = document.getElementById('userName');
+    const data = {
+        email: email
+    }
+    try {
+        const response = await fetch('http://localhost:3001/users/', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + accessToken
+            },
+            body: JSON.stringify(data)
+        }).then(function(response) {
+            return response.json();
+          }).then(function(data) {
+            console.log(data);
+            alert(data.message);
+          });
+    } catch (error) {
+        console.error('Error fetching profile: ', error);
+        // alert('An error occurred while loading profile. Please try again.');
+    }
+}
+
+async function logout() {
+    switchToLogin();
 }
