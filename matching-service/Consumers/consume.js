c402500e6f9f7fd59728bc33a9d6fd2bc2af4104
NIKHIL <e0648947@u.nus.edu>
2024-04-15 15:55:02 +0800

    edit

--- matching-service/Consumers/consume.js
+++ matching-service/Consumers/consume.js
@@ -45,10 +45,6 @@ function startConsumer(userData1, userData2, userMatch, res) {
             if(userMatch){
               //res.json({ message: "Match Found!!", userData2 });
               res.send("Match Found with user: "+ userData2.user+ "!!" );
-              if (consumer) {
-                channel.cancel(consumer.consumerTag);
-                console.log("Consumer stopped after user match.");
-              }
             }
     
           } else {
