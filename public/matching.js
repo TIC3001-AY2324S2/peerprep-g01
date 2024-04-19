c27943783b7e92bb9a2e3a4b2712cefcc58a230a
proseraju <ponnuroseraju@gmail.com>
2024-04-19 10:45:49 +0800

    update matching.js

--- public/matching.js
+++ public/matching.js
@@ -1,25 +1,4 @@
-//const { getUserProfile } = require('./login');
-//const userInfo = userLogin.data; // Call the function to populate data
-//const userInput = userInfo?.userDetails?.email
-
-/*getUserProfile().then(data => {
-    // Use data here
-    const userData = data.email;
-    userInput =  userData;
-    console.log("username in matching service:", userInput);
-});
-*/
-
-/*import {getUserProfile} from './login.js'
-userData = getUserProfile.data
-const userInput = userData.email;*/
-
-/*import userData from "./login";
-const userInput = userData;
-console.log("username in matching service:", userInput);*/
-
-document.getElementById('responseContent').style.display = 'none'; 
-
+document.getElementById('responseContent').style.display = 'none';
 
 document.addEventListener('DOMContentLoaded', function(event) {
     document.getElementById('start-Matching').addEventListener('click', startMatching);
@@ -34,18 +13,24 @@ function stopMatching(){
     document.getElementById("timer").style.display = "none"; // Hide the timer
     document.getElementById('feedback').style.display = 'none'; // Hide feedback
     //switchToMyProfile();
-
 }
 
 let countdownInterval; // Define countdownInterval globally to access it later
 
 function startMatching() {
     console.log(document.getElementById('start-Matching')); 
+    
 
     // Show feedback to the user
     document.getElementById('feedback').style.display = 'block';
 
-        setTimeout(() => {
+    // Initialize the timer
+    initializeTimer();
+    sendMessage();
+}
+
+function initializeTimer() {
+    setTimeout(() => {
         // Show the timer
         document.getElementById("timer").style.display = "block";
 
@@ -57,17 +42,11 @@ function startMatching() {
         countdownInterval = setInterval(function () {
             secondsLeft--;
             updateClock(secondsLeft);
-            if (secondsLeft <= 0) {
-                clearInterval(countdownInterval); // Stop the countdown when time is up
-                document.getElementById("timer").style.display = "none"; // Hide the timer
-                document.getElementById('feedback').style.display = 'none'; // Hide feedback
-                if (confirm("Time's up! Do you want to retry matching?")) {
-                    startMatching(); // Reload the page to retry matching
-                }
-            }
         }, 1000);
-    }, 100); // Wait 0.1 seconds before showing timer   
+    }, 100); // Wait 0.1 seconds before showing timer
+}
 
+function sendMessage(){
     //retrieve current user name
     const userInput = document.getElementById('username').value.trim();
     console.log("userinput is: " + userInput);
@@ -80,12 +59,10 @@ function startMatching() {
         .then(response => response.text())
         .then(data => {
             // Display data in modal body
+            alert(data);
             stopMatching();
-            document.getElementById('responseContent').style.display = 'block';
-            document.getElementById('responseContent').innerHTML = data;
-
-})
-    .catch(error => console.error('Error:', error));   
+        })
+        .catch(error => console.error('Error:', error));   
 }
 
 function updateClock(seconds) {
@@ -94,5 +71,3 @@ function updateClock(seconds) {
     const display = `${minutes < 10 ? '0' : ''}${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
     document.getElementById('clock').textContent = display;
 }
-
-
